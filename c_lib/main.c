5a8dcce5afa2f75536f1ad0de2e0b2f5c61e9aec
Edu <jonas.eduard.winkler@gmail.com>
2022-01-19 12:56:41 +0100

    removed obsolete code fragements | added list to list conversion | fixed function entry | allowed GIL to be released for threadsafe code

--- c_lib/main.c
+++ c_lib/main.c
@@ -47,18 +47,15 @@ static const char *test_wa[5] = {
         "Fünftes Element"
 };
 
+void *__strArrToPyLst(StringArray *stringArray, PyObject *pyList);
 
-PyObject *crw_get_info(PyObject *self, PyObject *args);
+void *__pyLstToStrArr(PyObject *list, StringArray **stringArray);
 
-void *strArrToPyLst(StringArray *stringArray, PyObject *pyList);
-
-str *get_info(str string, size_t amount_pattern);
-
-void *pyLstToStrArr(PyObject *list, StringArray **stringArray);
-
-void *mp_search(void *args);
+void *__mp_search(void *args);
 
 void *__prep_regex_pattern(StringArray *patternArray, Regex **re_patterns);
+
+void *__lstToPyLst(List *list, PyObject *pyList);
 
 void *receiving_function(PyObject *stringList, PyObject *patternList);
 
@@ -90,9 +87,9 @@ int main(void) {
         }
     }
     // todo error handling missing
-    strArrToPyLst(patternList, pyPattern);
+    __strArrToPyLst(patternList, pyPattern);
 
-    if (NULL != (error = strArrToPyLst(stringArray, pyList))) {
+    if (NULL != (error = __strArrToPyLst(stringArray, pyList))) {
         fprintf(stderr, "%s", error->err_msg);
         exit(-1);
     } else {
@@ -104,7 +101,7 @@ int main(void) {
         }
 
 //        StringArray *stringArray_one;
-//        if(NULL != (error = pyLstToStrArr(pyList, &stringArray_one))){
+//        if(NULL != (error = __pyLstToStrArr(pyList, &stringArray_one))){
 //            fprintf(stderr, "%s\n", error->err_msg);
 //        }else{
 //            // PyList successfully converted to StringArray ... proceed!
@@ -126,14 +123,15 @@ int main(void) {
     return 0;
 }
 
-void *strArrToPyLst(StringArray *stringArray, PyObject *pyList) {
-    /* Function: strArrToPyLst
+void *__strArrToPyLst(StringArray *stringArray, PyObject *pyList) {
+    /* Function: __strArrToPyLst
      * ---------------------------
      * Function converts an array of strings to a python list
      *
-     * str_arr: The array of strings to be converted
+     * stringArray: The array of strings to be converted
+     * pyList:      The Python-List object
      *
-     * returns: A python list object containing provided strings
+     * returns: Null if successful else Error
      */
     if (!pyList) {
         return error_new("Provided PyList-Object not valid", ERROR_CODE);
@@ -146,8 +144,8 @@ void *strArrToPyLst(StringArray *stringArray, PyObject *pyList) {
     return NULL;
 }
 
-void *pyLstToStrArr(PyObject *list, StringArray **stringArray) {
-    /*Function: pyLstToStrArr
+void *__pyLstToStrArr(PyObject *list, StringArray **stringArray) {
+    /*Function: __pyLstToStrArr
     * ---------------------------
     * Function converts a python list to an array of strings
     *
@@ -173,6 +171,30 @@ void *pyLstToStrArr(PyObject *list, StringArray **stringArray) {
     return error_new("Error creating StringArray from Python List-Object", ERROR_CODE);
 }
 
+void *__lstToPyLst(List *list, PyObject *pyList){
+    /*
+     * Function: __lstToPyLst
+     * ----------------------
+     * Function converts List of StringArrays to Python List of Lists
+     *
+     * list: The list-obj to be converted
+     * pyList: The python object to be created
+     *
+     * returns: NULL if successful else Error
+     */
+    if(!pyList){
+        return raise("Provided PyList object not valid");
+    }else if (!list){
+        return raise("Provided list object not valid");
+    }
+    for (long i = 0; i < list->length; i++){
+        PyObject *tmp_obj = PyList_New((long) list->list[i].max_length);
+        __strArrToPyLst(&list->list[i], tmp_obj);
+        PyList_SetItem(pyList, i, tmp_obj);
+    }
+    return NULL;
+}
+
 void *chunk_py_list(PyObject *list, List **param_list) {
     /*
      * Function: re..
@@ -184,7 +206,7 @@ void *chunk_py_list(PyObject *list, List **param_list) {
      */
     Error *error;
     StringArray *tmp_stringArray;
-    if (NULL != (error = pyLstToStrArr(list, &tmp_stringArray))) {
+    if (NULL != (error = __pyLstToStrArr(list, &tmp_stringArray))) {
         return error;
     }
     Py_CLEAR(list);
@@ -228,81 +250,101 @@ void *__prep_regex_pattern(StringArray *patternArray, Regex **re_patterns) {
 }
 
 void *receiving_function(PyObject *stringList, PyObject *patternList) {
-// todo Uncomment for actual program
+    START_PYTHON_CODE
+    __python_init__();
+    PyGILState_STATE *gil_state = malloc(sizeof(PyGILState_STATE));
+    __python_code_start__(gil_state);
 
-//    START_PYTHON_CODE
-//    __python_init__();
-//    PyGILState_STATE *gil_state = malloc(sizeof(PyGILState_STATE));
-//    __python_code_start__(gil_state);
     Error *error;
-    if (stringList == NULL || patternList == NULL ||
-        0 == PyObject_Length(patternList) || 0 == PyObject_Length(stringList)) {
-        error = raise("Illegal Argument!\nList Arguments must not be NULL or emtpy!");
-        goto finalize;
-    }
-    register size_t amount_pattern = PyObject_Length(patternList);
-
     StringArray *patternArray;
-    if (NULL != (error = pyLstToStrArr(patternList, &patternArray))) {
-        goto finalize;
-    }
-
     List *chunked_list;
 
-    if (NULL != (error = chunk_py_list(stringList, &chunked_list))) {
-        raise(error->err_msg);
+    {
+        // This Code-block must be GIL-safe !!
+        if (stringList == NULL || patternList == NULL ||
+            0 == PyObject_Length(patternList) || 0 == PyObject_Length(stringList)) {
+            error = raise("Illegal Argument!\nList Arguments must not be NULL or emtpy!");
+            goto finalize;
+        }
+        register size_t amount_pattern = PyObject_Length(patternList);
+
+        if (NULL != (error = __pyLstToStrArr(patternList, &patternArray))) {
+            goto finalize;
+        }
+
+        if (NULL != (error = chunk_py_list(stringList, &chunked_list))) {
+            raise(error->err_msg);
+        }
+        __python_code_end__(gil_state);
+        END_PYTHON_CODE
+        // End GIL-safe Code
     }
 
-    size_t      thread;
-    pthread_t  *thread_handles;
-    List       *thread_values = list_new(); //todo free
-    size_t      amount_chunks = chunked_list->length;
-    thread_handles = malloc(sizeof(pthread_t) * amount_chunks);
+    List       *thread_values = list_new();
 
-    // Init Mutext
-    if(pthread_mutex_init(&mutex, NULL) != 0){
-        // todo error occurred initializing mutex
-    }
+    Py_BEGIN_ALLOW_THREADS
 
-    ThreadData **args = malloc(amount_chunks * sizeof(ThreadData));
+        size_t      thread;
+        pthread_t  *thread_handles;
+        size_t      amount_chunks = chunked_list->length;
+        thread_handles = malloc(sizeof(pthread_t) * amount_chunks);
 
-    for (size_t i = 0; i < amount_chunks; i++){
-        ThreadData *arg = (ThreadData *) malloc(sizeof(ThreadData));
-        arg->input = &chunked_list->list[i];
-        arg->search_pattern = patternArray;
-        arg->output = thread_values;
-        args[i] = arg;
-    }
+        // Init Mutext
+        if(pthread_mutex_init(&mutex, NULL) != 0){
+            // todo error occurred initializing mutex
+        }
 
-    // Start threads
-    for (thread = 0; thread < amount_chunks; thread++) {
-        pthread_create(&thread_handles[thread], NULL, mp_search, (void *) args[thread]);
-    }
-    // Exit threads | retrieve collected data
-    for (thread = 0; thread < amount_chunks; thread++) {
-        pthread_join(thread_handles[thread], NULL);
-    }
-    // Free up Thread memory
-    free(thread_handles);
-    pthread_mutex_destroy(&mutex);
+        ThreadData **args = malloc(amount_chunks * sizeof(ThreadData));
 
-    for (size_t i = 0; i < amount_chunks; i++){
-        free(args[i]);
-    }
-    free(args);
+        for (size_t i = 0; i < amount_chunks; i++){
+            ThreadData *arg = (ThreadData *) malloc(sizeof(ThreadData));
+            arg->input = &chunked_list->list[i];
+            arg->search_pattern = patternArray;
+            arg->output = thread_values;
+            args[i] = arg;
+        }
 
-    list_print(thread_values, stdout);
+        // Start threads
+        for (thread = 0; thread < amount_chunks; thread++) {
+            pthread_create(&thread_handles[thread], NULL, __mp_search, (void *) args[thread]);
+        }
+        // Exit threads | retrieve collected data
+        for (thread = 0; thread < amount_chunks; thread++) {
+            pthread_join(thread_handles[thread], NULL);
+        }
+        // Free up Thread memory
+        free(thread_handles);
+        pthread_mutex_destroy(&mutex);
+
+        for (size_t i = 0; i < amount_chunks; i++){
+            free(args[i]);
+        }
+        free(args);
+
+        list_print(thread_values, stdout);
+
+    Py_END_ALLOW_THREADS
+
+    START_PYTHON_CODE
+    __python_code_start__(gil_state);
+
+    PyObject *returnLst = PyList_New((long) thread_values->length);
+    __lstToPyLst(thread_values, returnLst);
 
     finalize:
     if (patternArray) stringArray_del(patternArray);
     if (chunked_list) list_del(chunked_list);
-    //if (thread_values) list_del(thread_values);
+    if (thread_values) list_del(thread_values);
 
-    return NULL;
+    END_PYTHON_CODE
+    __python_code_end__(gil_state);
+    __python_des__();
+
+    return returnLst;
 }
 
-void *mp_search(void *args) {
-    /* Function: mp_search
+void *__mp_search(void *args) {
+    /* Function: __mp_search
      * -------------------
      * Execute regex-search on strings of provided input list
      *
@@ -330,9 +372,6 @@ void *mp_search(void *args) {
     if (NULL == thread_data->input){
         fprintf(stderr, "I'm just proceeding even though I should've stopped long ago... after __prep_regex\n");
     }
-//    fprintf(stdout, "\nStarting search function\n");
-//    stringArray_print(thread_data->input, stdout);
-//    fprintf(stdout, "\nEnding search function\n");
 
     for(size_t i = 0; i < thread_data->input->length; i++){
         StringArray *new_stringArray = stringArray_new(amount_pattern);
@@ -358,175 +397,3 @@ void *mp_search(void *args) {
     KILL_THREAD:
     pthread_exit(NULL);
 }
-
-/*
-void *mp_search(void *args) {
-     * Function: mp_search (Multiprocess-Search)
-     * -------------------
-     * Optimised function to regex_search multiple for multiple
-     * pattern in optionally multiple strings
-     *
-     * strings: array of strings
-     * chunk_size: The amount of strings provided
-     *
-     * raises: NULL if error occurred trying to match/compiling pattern
-     * returns: Array of String arrays containing matches
-     *
-     *                                     [match_pattern_one]
-     *          --> [abstract][pattern_one][match_pattern_two]
-     *                                     [match_pattern_...]
-
-
-    // Just for clarification (void cast)
-    ThreadData *thread_data = (ThreadData *) args;
-
-    for (size_t outer_idx = 0; outer_idx < thread_data->length; outer_idx++) {
-        thread_data->result[outer_idx] = malloc(len(patterns) * sizeof(char*));
-        for (size_t inner_idx = 0; inner_idx < len(patterns); inner_idx++) {
-            Re *regex = regex_search(regex_compile(patterns[inner_idx]), thread_data->cur_str_ptr[outer_idx]);
-
-            // If regex compilation NOT successful
-            // -or- if pattern was not found in string
-            // Concludes to ERROR !! -> cleanup -> exit
-            if (regex->err_code) {
-                write_regex_error(regex);
-                fprintf(stderr, "ERROR: Regex failed state! -> %s\n"
-                                "Pattern tried: %s\n"
-                                "String: %s", regex->err_msg, regex->pattern, thread_data->cur_str_ptr[outer_idx]);
-                free_regex(regex);
-                thread_data = NULL;
-                goto KILL_THREAD;
-            }
-
-            // If pattern was found
-
-            thread_data->result[outer_idx][inner_idx] = "Hello";
-            char *test = strdup(regex->match->group);
-            //thread_data->result[outer_idx][inner_idx] = strdup(regex->match->group);
-            printf("Inside Loop: %s\n", thread_data->result[outer_idx][inner_idx]);
-            free_regex(regex);
-        }
-    }
-    KILL_THREAD:
-    pthread_exit(NULL);
-}
-
-
-PyObject *crw_get_info(PyObject *self, PyObject *args) {
-     * Function: crw_get_info
-     * ----------------------
-     * Retrieve Python-List containing findings of regex_search-pattern in provided string
-     *
-     * string: The string to be searched
-     *
-     * returns: Python-List-Object
-     *          NULL if error occurred
-
-
-
-    START_PYTHON_CODE
-    // Python-GIL status
-    PyGILState_STATE *gil_state = malloc(sizeof(PyGILState_STATE));
-    // Allow Python code
-    init_python();
-    // Secure GIL
-    start_python_code(gil_state);
-
-
-
-    // todo Remove this immediately
-    StringArray *str_array = malloc(sizeof(StringArray));
-    str_array->string_array = malloc(5 * sizeof(char *));
-    str_array->string_array = (str *) test_string;
-    str_array->length = 5;
-    PyObject *list_one = strArrToPyLst(str_array);
-
-
-    // Get Python-list from args
-    // And convert list to array
-    {
-        PyObject *list = list_one; // todo should be empty see following if clause
-        StringArray *string_array;
-
-        //if(!PyArg_ParseTuple(args, "0", &list))
-        //    return NULL;
-        if ((string_array = pyLstToStrArr(list))) {
-            // End python Code release GIL
-            exit_python_code(gil_state);
-            {
-                END_PYTHON_CODE
-                Py_BEGIN_ALLOW_THREADS
-                    // Split the array into equal pieces
-                    size_t amount_chunks = string_array->length / CHUNK_SIZE;
-                    size_t left_over = string_array->length % CHUNK_SIZE;
-                    if (left_over) amount_chunks++;
-                    size_t last_element = amount_chunks -1;
-
-                    char ***ret_value = malloc(amount_chunks*sizeof(char**));
-
-                    size_t thread;
-                    pthread_t *thread_handles;
-                    thread_handles = malloc(sizeof(pthread_t) * amount_chunks);
-
-                    for (thread = 0; thread < amount_chunks; thread++) {
-                        ThreadData arg = {string_array->string_array+(thread*CHUNK_SIZE),
-                                          (thread != last_element || left_over == 0) ? CHUNK_SIZE : left_over,
-                                          ret_value+(thread*CHUNK_SIZE)};
-
-                        pthread_create(&thread_handles[thread], NULL, mp_search, (void *) &arg);
-                    }
-                    for (thread = 0; thread < amount_chunks; thread++) {
-                        pthread_join(thread_handles[thread], NULL);
-                    }
-                    free(thread_handles);
-
-                    for(thread = 0; thread < amount_chunks; thread++){
-                        for(size_t idx = 0; idx < (thread != last_element || left_over == 0) ? CHUNK_SIZE : left_over; idx++){
-                            for(size_t i = 0; i < len(patterns); i++){
-                                printf("Found: %s, Index: %lu", ret_value[thread][idx] ? ret_value[thread][idx] : i);
-                            }
-                        }
-                    }
-
-
-                Py_END_ALLOW_THREADS
-            }
-        }
-        START_PYTHON_CODE
-        start_python_code(gil_state);   // Lock GIL
-        Py_DecRef(list);                // Free Python-List Object
-    }
-    exit_python_code(gil_state);        // Release GIL (No use of Threads allowed however!)
-    END_PYTHON_CODE
-    destruct_python();
-}
-
-inline str *get_info(const str string, size_t amount_pattern) {
-     * Function: get_info
-     * ------------------
-     * Function to retrieve matching substrings found with
-     * provided patterns.
-     *
-     * string: The string to regex_search
-     * amount_pattern: The max amount of patterns initially created
-     *
-     * returns: An array of strings containing the results
-     *          If an error occurred while compiling or searching for substring
-     *          the error message is stored in the last position of the array!
-
-
-    str *ret_value = malloc((amount_pattern + 1) * sizeof(str));
-    for (size_t i = 0; i < amount_pattern; i++) {
-        Re *regex = regex_search(regex_compile(patterns[i]), (str) string);
-        if (regex->err_code) {
-            write_regex_error(regex);
-            ret_value[amount_pattern] = strdup(regex->err_msg);
-            free_regex(regex);
-            return ret_value;
-        }
-        ret_value[i] = strdup(regex->match->group);
-        free_regex(regex);
-    }
-    return ret_value;
-}
-*/
